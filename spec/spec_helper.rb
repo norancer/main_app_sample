3e28ec1b7d851c54a009bdcf29e2afc193997f85
norancer <hirao@sitebridge.co.jp>
2015-08-03 13:42:09 +0900

    fix guard rspec & rubocop

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,11 +1,6 @@
-ENV['RAILS_ENV'] ||= 'test'
-require File.expand_path('../../config/environment', __FILE__)
-require 'rspec/rails'
-
-Dir[Rails.root.join('spec/support/**/*.rb')].each { |f| require f }
-
+require 'factory_girl_rails'
 RSpec.configure do |config|
-  config.include Devise::TestHelpers, type: :controller
-  config.extend ControllerMacros, type: :controller
+  # config.include Devise::TestHelpers, type: :controller
+  # config.extend ControllerMacros, type: :controller
   config.include FactoryGirl::Syntax::Methods
 end
